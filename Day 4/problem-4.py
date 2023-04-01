77adc64a17c5feac7fb9683746ab7dde380516e2
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2023-04-01 14:58:03 -0300

    Day 4
    - Utilizaão de 'Enclosure' para definição de métodos que retornam métodos e os salvam em variáveis
    - Utilização de 'lambda functions' para a chamada dos métodos

---
+++ Day 4/problem-4.py
@@ -0,0 +1,17 @@
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+
+def car(pair):
+    return pair(lambda a, b: a)
+
+
+def cdr(pair):
+    return pair(lambda a, b: b)
+
+
+pair = cons(3, 4)
+print(car(pair))
+print(cdr(pair))
