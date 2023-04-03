ccbc5edbad7d64e3b8ba4820f396ce53f0ebbd14
Gabriel Camatta Zanotelli <55805329+Zanotelli@users.noreply.github.com>
2023-04-02 21:06:43 -0300

    Help problem
    - Resolução exemplo
    - Tentar resolver em uma data posterior

---
+++ Help question/help_question-hard.py
@@ -0,0 +1,26 @@
+# The question we'll work through is the following:
+# return a new sorted merged list from K sorted lists,
+# each with size N
+import heapq
+
+
+def merge(lists):
+    merged_list = []
+
+    heap = [(lst[0], i, 0) for i, lst in enumerate(lists) if lst]
+    heapq.heapify(heap)
+
+    while heap:
+        val, list_ind, element_ind = heapq.heappop(heap)
+
+        merged_list.append(val)
+
+        if element_ind + 1 < len(lists[list_ind]):
+            next_tuple = (lists[list_ind][element_ind + 1],
+                          list_ind,
+                          element_ind + 1)
+            heapq.heappush(heap, next_tuple)
+    return merged_list
+
+
+print(merge([[10, 15, 30], [12, 15, 20], [17, 20, 32]]))
